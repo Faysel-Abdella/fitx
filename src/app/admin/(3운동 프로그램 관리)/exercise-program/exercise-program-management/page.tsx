adae7c197450981d5f9bbd17b5ea0f59d95139b2
MubarakAd <mubaarakadem@gmail.com>
2025-02-04 21:04:01 +0300

    commit to pull

--- src/app/admin/(3운동 프로그램 관리)/exercise-program/exercise-program-management/page.tsx
+++ src/app/admin/(3운동 프로그램 관리)/exercise-program/exercise-program-management/page.tsx
@@ -1,8 +1,14 @@
+'use client'
 import Header from "@/components/Header";
 import { Button } from "@heroui/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const ExerciseProgramManagement = () => {
+  const router=useRouter()
+  const handleRoute=()=>(
+    router.push("/admin/exercise-program/exercise-program-management/addTemplate")
+  )
   return <div>
     <Header title="템플릿 관리"/>
     <div className="flex justify-between items-center mt-5">
@@ -10,7 +16,12 @@ const ExerciseProgramManagement = () => {
         <h1 className="text-black">muba</h1>
  <button className="py-[10px] px-[20px] bg-[#4D4D4D] rounded-[5px]">삭제</button> 
       </div>
-      <button className="py-[15px] rounded-[100px] bg-[#006BFF] w-[270px]">운동 템플릿 추가</button>
+      <button onClick={handleRoute} className="py-[15px] rounded-[100px] bg-[#006BFF] w-[270px]">운동 템플릿 추가</button>
+    </div>
+    <div className="flex flex-col mt-[10px]">
+      <div className=" flex gap-[10px]">
+
+      </div>
     </div>
   </div>;
 };
