2c23463861d7f00eebfe02af6a02595865b8eae1
MubarakAd <mubaarakadem@gmail.com>
2025-02-04 20:49:01 +0300

    commit to pull

--- src/app/admin/(3운동 프로그램 관리)/exercise-program/exercise-program-management/page.tsx
+++ src/app/admin/(3운동 프로그램 관리)/exercise-program/exercise-program-management/page.tsx
@@ -1,7 +1,18 @@
+import Header from "@/components/Header";
+import { Button } from "@heroui/react";
 import React from "react";
 
 const ExerciseProgramManagement = () => {
-  return <div>ExerciseProgramManagement</div>;
+  return <div>
+    <Header title="템플릿 관리"/>
+    <div className="flex justify-between items-center mt-5">
+      <div className="flex gap-5">
+        <h1 className="text-black">muba</h1>
+ <button className="py-[10px] px-[20px] bg-[#4D4D4D] rounded-[5px]">삭제</button> 
+      </div>
+      <button className="py-[15px] rounded-[100px] bg-[#006BFF] w-[270px]">운동 템플릿 추가</button>
+    </div>
+  </div>;
 };
 
 export default ExerciseProgramManagement;
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+interface headerProps {
+  title: string;
+}
+const Header = ({title}:headerProps) => {
+  return (
+    <div className="flex flex-col gap-[40px]">
+      <div className="flex justify-end gap-[24px]">
+        <p className="underline underline-offset-1 text-[#007AFF]">
+          세션 연장 새로고침
+        </p>
+        <p className="text-[#A0A0A0]">로그아웃</p>
+      </div>
+      <div>
+        <h1 className="font-bold text-[30px] text-[#4D4D4D]">{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
