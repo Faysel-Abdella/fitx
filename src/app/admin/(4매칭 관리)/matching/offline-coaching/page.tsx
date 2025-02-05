7a9a1dd1f3bf044419dd37c0da66a8aa2c40b604
Ziyad Hasen <hziyad933@gmail.com>
2025-02-05 01:08:20 -0800

    the skeleton is done

---
+++ src/app/admin/(4매칭 관리)/matching/offline-coaching/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OfflineCoaching = () => {
+  return <div>OfflineCoaching</div>;
+};
+
+export default OfflineCoaching;
---
+++ src/app/admin/(5대회관리)/competition-management/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompetitionManagement = () => {
+  return <div>CompetitionManagement</div>;
+};
+
+export default CompetitionManagement;
